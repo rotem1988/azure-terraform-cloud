6f03b64aa8d37ebfc56e0a6e3255988b611b26a6
u725205 <Rotem.Hochman@dh.com>
2021-04-20 09:46:20 +0300

    added main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+
+ backend "remote" {
+   organization = "rotem-hochman"
+   workspaces {
+     name = "azure-terraform-cloud"
+   }
+ }
+}
+
+provider "azurerm" {
+  features {}
+}
